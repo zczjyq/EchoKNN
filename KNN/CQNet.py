f9e2123de911aaf98efa80d07a33d0a29e47746e
zjlhui <3193796314@qq.com>
2024-11-09 21:37:58 +0800

    读取指定的图片

--- KNN/CQNet.py
+++ KNN/CQNet.py
@@ -105,6 +105,23 @@ def reshape_img_matrix(img_8bit_matrix, train_num):
 
     return reshaped_arrays
 
+def reshape_img_matrix_single(img_8bit_matrix, photo_show):
+    # 确保 img_8bit_matrix 的大小正确
+    if img_8bit_matrix.shape[0] != 307200:
+        raise ValueError("img_8bit_matrix must have shape (307200, 500)")
+
+    # 检查是否传入有效的 photo_show 值
+    if photo_show <= 0 or photo_show > img_8bit_matrix.shape[1]:
+        raise ValueError("photo_show must be within the range of available images")
+
+    # 将指定图片重塑为 (600, 512) 的二维数组
+    reshaped_array = img_8bit_matrix[:, photo_show-1].reshape((600, 512))
+
+    # 翻转操作 (垂直翻转)
+    reshaped_array = np.flip(reshaped_array, axis=0)
+
+    return reshaped_array
+
 
 def normalize_data(data):
     return (data - np.min(data)) / (np.max(data) - np.min(data))
@@ -292,81 +309,3 @@ def prepare_data_and_labels(ans, train_num, alpha_start=0.5, alpha_end=2.5, alph
     labels_tensor = labels_tensor.unsqueeze(1)
 
     return data_tensor, labels_tensor
-
-
-
-# def read_sonar_data(file_path, nums_photo):
-#     # Read sonar beam data from a .dat file and return a matrix of image data
-#     valid_ping_num = nums_photo  # Number of valid pings
-#     num_IMG = 307200  # Total image data points per ping
-
-#     img_8bit_matrix = np.zeros((num_IMG, valid_ping_num), dtype=np.uint8)  # Initialize the output matrix
-
-#     with open(file_path, 'rb') as file:
-#         file.read(40)  # Read and discard the file header (10 uint32)
-
-#         # Fixed header search
-#         c1 = struct.unpack('B', file.read(1))[0]
-#         c2 = struct.unpack('B', file.read(1))[0]
-#         c3 = struct.unpack('B', file.read(1))[0]
-#         for k in range(valid_ping_num):
-#             if file.read(0) == 'b':
-#                 break
-#             # Header search within 64k
-#             for kk in range(65535):
-#                 c4 = struct.unpack('B', file.read(1))[0]
-#                 if c4 == ord('O') and c3 == ord('S') and c2 == 0 and c1 == 0:
-#                     break
-#                 c1, c2, c3 = c2, c3, c4
-
-#                 if file.read(0) == 'b':
-#                     return img_8bit_matrix
-
-#             if kk == 65535:
-#                 return img_8bit_matrix
-#             file.read(14)  # Read and discard SHeaderStr
-
-#             mode = np.frombuffer(file.read(5), dtype=np.uint8)
-#             range_percent = struct.unpack('d', file.read(8))[0]
-#             gain_percent = struct.unpack('d', file.read(8))[0]
-#             speed_of_sound = struct.unpack('d', file.read(8))[0]
-#             salinity = struct.unpack('d', file.read(8))[0]
-#             ext_flags = struct.unpack('I', file.read(4))[0]
-#             reserved = np.frombuffer(file.read(32), dtype=np.uint32)
-
-#             num_dat16 = reserved[4]  # Sample points
-#             chn_dat16 = reserved[5]  # Channels
-
-#             ping_id = struct.unpack('I', file.read(4))[0]
-#             status = np.frombuffer(file.read(69), dtype=np.uint8)
-#             range_resolution = struct.unpack('d', file.read(8))[0]
-#             n_ranges = struct.unpack('H', file.read(2))[0]
-#             n_beams = struct.unpack('H', file.read(2))[0]
-#             file.read(28)  # Read and discard spare
-
-#             # Update sample points and beams
-#             num_sam = n_ranges
-#             num_beam = float(n_beams)
-
-#             file.read(1024)  # Read and discard beam angles (512 int16)
-#             file.read(822)  # Read and discard tem16 (411 uint16)
-
-#             # Read image data
-#             img_8bit = np.frombuffer(file.read(num_IMG), dtype=np.uint8)
-#             img_8bit_matrix[:, k] = img_8bit
-
-#     return img_8bit_matrix
-
-
-# def reshape_img_matrix(img_8bit_matrix, nums_photo):
-#     # Ensure img_8bit_matrix has the shape (307200, 500)
-#     if img_8bit_matrix.shape != (307200, nums_photo):
-#         raise ValueError("img_8bit_matrix must have shape (307200, 500)")
-
-#     # Reshape each column into a (600, 512) array
-#     reshaped_arrays = np.empty((600, 512, img_8bit_matrix.shape[1]), dtype=np.uint8)
-
-#     for k in range(img_8bit_matrix.shape[1]):
-#         reshaped_arrays[:, :, k] = img_8bit_matrix[:, k].reshape((600, 512))
-
-#     return reshaped_arrays
